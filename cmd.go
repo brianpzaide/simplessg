3e6732f95526a4686dd1bb42ebcabba4395f0427
brianpzaide <brianpzaide@gmail.com>
2026-09-16 10:57:19 +0530

    fix: create new blogpost and the assets folder for this new blog post at correct place i.e inside the 'posts/' folder

--- cmd.go
+++ cmd.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"path/filepath"
 	"time"
 
 	"github.com/gosimple/slug"
@@ -23,14 +24,18 @@ func handleNewBlogPost(ctx context.Context, c *cli.Command) error {
 	}
 
 	generatedSlug := slug.Make(title)
-	postFileName := fmt.Sprintf("%s.md", generatedSlug)
+	wd, err := os.Getwd()
+	if err != nil {
+		fmt.Println("could not get the current working directory in cmd: handleNewBlogPost")
+	}
+	postFileName := filepath.Join(wd, "posts", fmt.Sprintf("%s.md", generatedSlug))
 	fmt.Printf("Creating post %s...\n", postFileName)
 
 	err = os.WriteFile(postFileName, content, 0777)
 	if err != nil {
 		return err
 	}
-	assetsDir := fmt.Sprintf("assets/%s", generatedSlug)
+	assetsDir := filepath.Join(wd, "posts", "assets", generatedSlug)
 	fmt.Printf("Creating assets directory %s...\n", assetsDir)
 	err = os.MkdirAll(assetsDir, 0777)
 	if err != nil {
